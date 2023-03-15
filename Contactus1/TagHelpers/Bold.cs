4d8905a2a1012ff0f250124fd5a65bff9913940f
Amirhossein <amirhossein.mokhtari120@gmail.com>
2023-03-15 12:44:02 +0330

    Added-TagHelper2
    Add Tag Helper call PhoneNumber Bold Tag View In .Net

---
+++ Contactus1/TagHelpers/Bold.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Contactus1.TagHelpers
+{
+    [HtmlTargetElement(Attributes = "bold")]
+    public class Bold:TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.PreContent.AppendHtml("<strong>");
+            output.PostContent.AppendHtml("</strong>");
+            output.Attributes.Add("stylde", "font-size:48px");
+            base.Process(context, output);  
+        }
+    }
+}
---
+++ Contactus1/TagHelpers/PhoneTagHelper.cs
@@ -0,0 +1,20 @@
+﻿ using Microsoft.AspNetCore.Razor.Runtime.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Contactus1.TagHelpers
+{
+    // You may need to install the Microsoft.AspNetCore.Razor.Runtime package into your project
+    [HtmlTargetElement("call",TagStructure =TagStructure.NormalOrSelfClosing)]
+    public class PhoneTagHelper : TagHelper
+    {
+        public string PhoneNumber { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            output.Attributes.Add("href",$"tell {PhoneNumber}" );
+            output.Content.AppendHtml($"Call{ PhoneNumber}");
+            base.Process(context,output);
+
+        }
+    }
+}
