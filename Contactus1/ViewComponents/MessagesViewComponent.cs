f90055e43b3c1162bc172437b4e36a247b3bfe8d
Amirhossein <amirhossein.mokhtari120@gmail.com>
2023-03-15 21:34:35 +0330

    ViewComponent-Added

---
+++ Contactus1/ViewComponents/MessagesViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Contactus1.DataBase;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactUs.ViewComponents
+{
+    public class MessagesViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return await Task.FromResult(View("TableMessage",DataBase.Messagess));
+        }
+    }
+}
